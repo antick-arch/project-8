755fdb0a70aecef578b5c3f5bbe75a973110a505
antick-arch <e241003@grad.iiuc.ac.bd>
2026-02-18 23:49:11 +0600

    added login page

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+document.getElementById("login-btn").addEventListener('click',function(){
+    const inputTxt = document.getElementById("inputTxt");
+    const usr = inputTxt.value;
+    console.log(usr);
+    const inputPass = document.getElementById("inputPass");
+    const pass = inputPass.value;
+    console.log(pass);
+
+    if(usr === '01234567890' && pass === '1234'){
+        alert("Login Successful");
+        window.location.assign("/home.html");
+    }
+    else{
+        alert("Try Again");
+    }
+})
